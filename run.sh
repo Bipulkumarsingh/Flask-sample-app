e2ccd25f73a09ddaf0a401546a262c965108054c
Bipul Kumar Singh <bksingh@netlink.com>
2021-04-02 19:07:31 +0530

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-gunicorn -b 127.0.0.1:8080 --worker-class gevent -w 2 --threads 2 app:app --max-requests-jitter 200 --max-requests 150 --reload
+gunicorn -b 0.0.0.0:8080 --worker-class gevent -w 2 --threads 2 app:app --max-requests-jitter 200 --max-requests 150 --reload
